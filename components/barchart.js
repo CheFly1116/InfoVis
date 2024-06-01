dc4ca2e1222f43b0135e192b2f3d3418dccd5a00
CheFly1116 <sam4363@naver.com>
2024-06-01 11:08:09 +0900

    barchart.js

---
+++ components/barchart.js
@@ -0,0 +1,30 @@
+class Barchart {
+    margin = {
+        top: 10, right: 10, bottom: 40, left: 40
+    }
+
+    constructor(svg, width = 250, height = 250) {
+        this.svg = svg;
+        this.width = width;
+        this.height = height;
+    }
+
+    initialize() {
+        this.svg = d3.select(this.svg);
+        this.container = this.svg.append("g");
+        this.xAxis = this.svg.append("g");
+        this.yAxis = this.svg.append("g");
+        this.legend = this.svg.append("g");
+
+        this.xScale = d3.scaleBand();
+        this.yScale = d3.scaleLinear();
+
+        this.svg
+            .attr("width", this.width + this.margin.left + this.margin.right)
+            .attr("height", this.height + this.margin.top + this.margin.bottom);
+
+        this.container.attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+
+    }
+    
+}
