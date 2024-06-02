c5515df5591bc223928e4ee48e9d83eb0a2b15b6
CheFly1116 <sam4363@naver.com>
2024-06-03 02:48:18 +0900

    final

--- components/correlogram.js
+++ components/correlogram.js
@@ -1,6 +1,6 @@
-class Heatmap {
+class Correlogram {
     margin = {
-        top: 80, right: 25, bottom: 30, left: 40
+        top: 20, right: 20, bottom: 20, left: 20
     }
 
     constructor(svg, width = 1200, height = 1200) {
@@ -15,6 +15,8 @@ class Heatmap {
         this.xAxis = this.svg.append("g");
         this.yAxis = this.svg.append("g");
         
+        this.color = d3.scaleLinear();
+        this.size = d3.scaleSqrt();
         this.xScale = d3.scaleBand();
         this.yScale = d3.scaleBand();
 
@@ -27,8 +29,14 @@ class Heatmap {
     }
     update(data) {
         
-        const Groups = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment", "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "attack", "level"]
-        const color = d3.scaleDiverging([-1, 0, 1], d3.interpolateRdBu);
+        const Groups = ["duration", "src_bytes", "dst_bytes", "land", "wrong_fragment", "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "level"]
+        this.color
+            .domain([-1, 0, 1])
+            .range(["#B22222", "#fff", "#000080"]);
+        this.size
+            .domain([0, 1])
+            .range([0, 9]);
+        
         this.xAxis
             .attr("transform", `translate(${this.margin.left}, ${this.margin.top + this.height})`)
             .transition()
